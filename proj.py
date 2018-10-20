7c067cb756fcb46124165aee4b588b632c125687
Unknown <miguel.valerio@tecnico.ulisboa.pt>
2018-10-20 15:19:32 +0100

    4 wrong answer

--- proj.py
+++ proj.py
@@ -10,7 +10,7 @@ from search import *
 
 
 
-""" ============================ Tipo content ============================ """
+""" ============================= Tipo content ============================= """
 
 def c_peg():
     """ Retorna o simbolo representante de uma posicao ocupada. """
@@ -53,11 +53,11 @@ def is_blocked(e):
             False -> Caso contrario. """
     return e == c_blocked()
 
-""" ======================================================================= """
+""" ======================================================================== """
 
 
 
-""" ============================== Tipo pos ============================== """
+""" =============================== Tipo pos =============================== """
 
 def make_pos(l, c):
     """ Argumento: l, c
@@ -103,12 +103,35 @@ def pos_get_side(pos, side):
         return make_pos(pos_l(pos), pos_c(pos)-1)
 
 
+def pos_get_opposite_side(pos, side):
+    """ Argumentos: pos, side
+            pos  -> Posicao no tabuleiro.
+            side -> Direcao oposta da posicao vizinha.
+                   (0=>cima; 1=>direita; 2=>baixo; 3=>esquerda).
+        Retorno: pos_opposite_side
+            pos_opposite_side -> Posicao vizinha da posicao dada segundo a
+                                 direcao oposta da direcao dada. """
+    if side == 0:
+        return pos_get_side(pos, 2)
+    elif side == 1:
+        return pos_get_side(pos, 3)
+    elif side == 2:
+        return pos_get_side(pos, 0)
+    elif side == 3:
+        return pos_get_side(pos, 1)
+
+
 def pos_dif(pos_initial, pos_final):
+    """ Argumentos: pos_initial, pos_final
+            pos_initial -> Posicao no tabuleiro.
+            pos_final   -> Posicao no tabuleiro.
+        Retorno: dif
+            dif -> Posicao que representa a diferenca entre as posicoes dadas. """
     d_l = pos_l(pos_final) - pos_l(pos_initial)
     d_c = pos_c(pos_final) - pos_c(pos_initial)
     return make_pos(d_l, d_c)
 
-""" ======================================================================= """
+""" ======================================================================== """
 
 
 
@@ -141,7 +164,13 @@ def move_final(move):
     return move[1]
 
 
-def move_get_direction(move):   # 0=>cima; 1=>direita; 2=>baixo; 3=>esquerda
+def move_get_direction(move):
+    """ Argumento: move
+            move -> Jogada no jogo Solitaire.
+        Retorno: dir
+            dir -> Direcao da jogada dada.
+                   (0=>cima; 1=>direita; 2=>baixo; 3=>esquerda). """
+
     dif = pos_dif(move_initial(move), move_final(move))
 
     d_line = pos_l(dif)
@@ -159,13 +188,21 @@ def move_get_direction(move):   # 0=>cima; 1=>direita; 2=>baixo; 3=>esquerda
 
 
 def move_eat(move, pcom):
-    return pos_l(move_initial(move)) == pos_l(pcom) and pos_c(move_initial(move)) == pos_c(pcom)
+    """ Argumentos: move, pcom
+            move -> Jogada no jogo Solitaire.
+            pcom -> Posicao no tabuleiro.
+        Retorno: True, False
+            True  -> Se o move dado come a peca na posicao dada.
+            False -> Caso contrario. """
+    moveDir = move_get_direction(move)
+    mid = pos_get_side(move_initial(move), moveDir)
+    return pos_l(mid) == pos_l(pcom) and pos_c(mid) == pos_c(pcom)
 
-""" ======================================================================= """
+""" ======================================================================== """
 
 
-""" ============================= Tipo board ============================= """
 
+""" ============================== Tipo board ============================== """
 
 def board_set_pos(board, posit, content):
     """ Argumentos: board, posit, content
@@ -173,8 +210,8 @@ def board_set_pos(board, posit, content):
             posit   -> Posicao.
             content -> Conteudo a colocar na posicao."""
     board[pos_l(posit)][pos_c(posit)] = content
-    
-    
+
+
 def board_get_content(board, posit):
     """ Argumentos: board, posit
             board   -> Representa o tabuleiro de um jogo de Solitaire.
@@ -230,7 +267,7 @@ def board_moves(board):
 
             if is_peg(board_get_content(board, pos)):
                 movs += board_moves_pos(board, pos)
-                """for i in range(4):
+                """for i in range(4):   -> apenas para ver se a nova funcao funciona bem
                     pos_side = pos_get_side(pos, i)
                     pos_final = pos_get_side(pos_side, i)
 
@@ -241,6 +278,12 @@ def board_moves(board):
 
 
 def board_moves_pos(board, pos):
+    """ Argumentos: board, pos
+            board -> Representa o tabuleiro de um jogo de Solitaire.
+            pos   -> Posicao no tabuleiro que contenha uma peca.
+        Retorno: movs
+            movs -> Lista com os movimentos possiveis da peca dada no
+                    tabuleiro dado. """
     movs = []
     for i in range(4):
         pos_side = pos_get_side(pos, i)
@@ -260,10 +303,9 @@ def board_perform_move(board, move):
             result -> tabuleiro resultante de aplicar o move ao board."""
     first = move_initial(move)
     last = move_final(move)
-    midL = (pos_l(first)+pos_l(last))//2
-    midC = (pos_c(first)+pos_c(last))//2
 
-    mid = make_pos(midL, midC)
+    moveDir = move_get_direction(move)
+    mid = pos_get_side(first, moveDir)
 
     result = []
     for line in board:
@@ -277,6 +319,10 @@ def board_perform_move(board, move):
 
 
 def board_get_npeg(board):
+    """ Argumento: board
+            board -> Representa o tabuleiro de um jogo de Solitaire.
+        Retorno: nPeg
+            nPeg -> Numero de pecas existentes no tabuleiro dado. """
     npegs = 0
     nLines = board_get_NLines(board)
     nColumns = board_get_Ncolumns(board)
@@ -288,9 +334,11 @@ def board_get_npeg(board):
                 npegs += 1
     return npegs
 
+""" ======================================================================== """
 
-""" ======================================================================= """
 
+
+""" ============================ Tipo sol_state ============================ """
 
 class sol_state:
     def __init__(self, board, npeg=None, moves=None, nMoves=None):
@@ -309,23 +357,44 @@ class sol_state:
             self.nMoves = nMoves
 
 
-    def __lt__(self, other):  # self<other
+    def __lt__(self, other):
+        """ Argumentos: self, other
+                self  -> Instancia do estado de um jogo de Solitaire.
+                other -> Instancia do estado de um jogo de Solitaire.
+            Retorno: True, False
+                True  -> Se self < other.
+                False -> Caso contrario. """
         return self.npeg > other.npeg   # FIXME -> se comecar a dar merda entoa falta comparar os moves
 
+""" ======================================================================= """
 
+
+
+""" ============================ Tipo solitaire ============================ """
 
 class solitaire(Problem):
     def __init__(self, board):
         Problem.__init__(self, sol_state(board))
 
     def actions(self, state):
+        """ Argumentos: self, state
+                self  -> Representacao de um jogo de Solitaire.
+                state -> Representacao do estado de um jogo de Solitaire.
+            Retorno: moves
+                moves -> Movimentos possiveis do jogo dado estando no estado
+                         dado. """
         return state.moves
 
     def result(self, state, action):
-        # Pre-comida
-
+        """ Argumentos: self, state, action
+                self   -> Representacao de um jogo de Solitaire.
+                state  -> Representacao do estado de um jogo de Solitaire.
+                action -> Jogada no jogo Solitaire.
+            Retorno: newState
+                newState -> Estado resultante de executar a acao dada estando
+                            no estado dado. """
+        # ----------------------------- Pre-comida -----------------------------
         pos_ini = move_initial(action)
-        pos_fin = move_final(action)
 
         dir = move_get_direction(action)
         if dir == 0:
@@ -351,55 +420,54 @@ class solitaire(Problem):
         moves = []
         nMoves = 0
         for m in state.moves:
-            if move_initial(m) != pcom and move_initial(m) != pos_ini and m != mv1 and m != mv2 and move_eat(m, pos_ini):
-                """ move_initial(m) == pos_ini -> moves que podia fazer
-                    move_initial(m) == pcom -> moves que a comida podia fazer
-                    m != mv1 and m != mv2 -> moves que faziam sobre a comida"""
+            if move_initial(m) != pos_ini and not move_eat(m, pos_ini) and move_initial(m) != pcom and m != mv1 and m != mv2:
+                """ move_initial(m) == pos_ini -> moves que podia fazer.
+                    move_eat(m, pos_ini) -> moves que me comiam.
+                    move_initial(m) == pcom -> moves que a comida podia fazer.
+                    mv1 and mv2 -> moves que podiam comer o que comi.
+                  |------------------------------------------------------------|
+                                                |
+                                    esta-se a tirar estes moves. """
                 moves += [m.copy()]
                 nMoves += 1
 
+        # Faz a acao dada
         newBoard = board_perform_move(state.board, action)
 
-        # Pos-comida
+        # ----------------------------- Pos-comida -----------------------------
+        pos_fin = move_final(action)
 
         # moves que posso fazer
-        mvs = board_moves_pos(newBoard, move_final(action))
-
+        mvs = board_moves_pos(newBoard, pos_fin)
         moves += mvs
         nMoves += len(mvs)
 
         # moves que me podem comer
-        posU = pos_get_side(pos_fin, 0)
-        posR = pos_get_side(pos_fin, 1)
-        posD = pos_get_side(pos_fin, 2)
-        posL = pos_get_side(pos_fin, 3)
+        for i in range(4):
+            pos_side = pos_get_side(pos_fin, i)
+            pos_side_final = pos_get_opposite_side(pos_fin, i)
 
-        if is_peg(board_get_content(newBoard, posU)):
-            if is_empty(board_get_content(newBoard, posD)):
-                moves += [make_move(posU, posD)]
+            if is_peg(board_get_content(newBoard, pos_side)) and is_empty(board_get_content(newBoard, pos_side_final)):
+                moves += [make_move(pos_side, pos_side_final)]
                 nMoves += 1
-
-        if is_peg(board_get_content(newBoard, posD)):
-            if is_empty(board_get_content(newBoard, posU)):
-                moves += [make_move(posD, posU)]
-                nMoves += 1
-
-        if is_peg(board_get_content(newBoard, posR)):
-            if is_empty(board_get_content(newBoard, posL)):
-                moves += [make_move(posR, posL)]
-                nMoves += 1
-
-        if is_peg(board_get_content(newBoard, posL)):
-            if is_empty(board_get_content(newBoard, posR)):
-                moves += [make_move(posL, posR)]
-                nMoves += 1
-
 
         return sol_state(newBoard, state.npeg-1, moves, nMoves)
 
     def goal_test(self, state):
+        """ Argumentos: self, state
+                self  -> Representacao de um jogo de Solitaire.
+                state -> Representacao do estado de um jogo de Solitaire.
+            Retorno: True, False
+                True  -> Se o estado dado for o estado objetivo do jogo dado.
+                False -> Caso contratio. """
         return state.npeg == 1
 
-    def h(self, node):
+    def h(self, node):  # FIXME -> precisa de melhoras
+        """ Argumentos: self, node
+                self -> Representacao de um jogo de Solitaire.
+                node -> Representacao de um no das arvores de procura.
+            Retorno: h(node)
+                h(node) -> Valor atribuido pela funcao heuristica ao no dado. """
         return node.state.npeg
 
+""" ======================================================================= """
